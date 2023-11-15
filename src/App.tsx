a644e8903d381633cbfe766b0a826af269e35c1a
mirza <mirza.akhena@gmail.com>
2023-11-16 02:53:19 +0700

    move to case12

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import Page from "./cases/case10_exclusive_switch_with_form/page/Page";
-import Page from "./cases/case06_combine_03_04_05/page/Page";
+import Page from "./cases/case12_combine_03_04_05_11/page/Page";
 // import Page from "./cases/case11_layout/page/Page";
 const App = () => {
   //
