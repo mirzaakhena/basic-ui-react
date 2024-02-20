f8e2b4d8aa2ec370792a66c0d4df9dbc08690500
mirza <mirza.akhena@gmail.com>
2024-02-20 11:04:51 +0700

    first commit controller ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-// import Page from "./cases/case01_form/page/Page";
-// import Page from "./cases/case10_exclusive_switch_with_form/page/Page";
-// import Page from "./cases/case11_layout/page/Page";
-// import Page from "./cases/case12_combine_03_04_05_11_old/page/Page";
-// import Page from "./cases/case13_combine_03_04_05_11_thenew/page/Page";
-// import Page from "./cases/case15_integrating_tablecomponent/page/Page";
-// import Page from "./cases/case16_tablecomponent/page/Page";
-import Page from "./cases/case17_refactorlayout/page/Page";
+import Page from "./page/Page";
 
 const App = () => {
   //
